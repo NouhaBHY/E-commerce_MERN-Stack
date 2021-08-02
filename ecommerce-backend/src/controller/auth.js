55135753767909b7f0e36b46c870aa92cdee83b4
Your Name <youremail@yourdomain.com>
2021-08-02 21:28:25 +0200

    saving to Github

--- ecommerce-backend/src/controller/auth.js
+++ ecommerce-backend/src/controller/auth.js
@@ -1,7 +1,10 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken'); 
 
+
 exports.signup = (req, res) => {
+
+
     User.findOne({ email: req.body.email })
     .exec((error, user) => {
         if(user) return res.status(400).json({ // usser exist
