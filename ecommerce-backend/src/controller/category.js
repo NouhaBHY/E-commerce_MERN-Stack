c8dd660eb64024722494649019e91aaa7a1fc261
NouhaBHY <noablhy@gmail.com>
2021-08-11 17:20:58 +0200

    backend

--- ecommerce-backend/src/controller/category.js
+++ ecommerce-backend/src/controller/category.js
@@ -22,25 +22,6 @@ function createCategories(categories, parentId = null){
     }
     return categoryList;
 };
-/*
-exports.addCategory = (req, res) => {
-    const categoryObj = {
-        name: req.body.name,
-        slug: slugify(req.body.name)
-        }
-        if(req.body.parentId){
-            categoryObj.parentId = req.body.parentId;
-        }
-        const cat = new Category(categoryObj);
-        cat.save((error, category) => {
-            if(error) return res.status(400).json({ error});
-            if(category){
-                return res.status(201).json({ category });
-            }
-        });
-    
-}*/
-
 exports.addCategory = (req, res) => {
 
     /*
